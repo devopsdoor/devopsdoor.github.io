0aa231ec17487d26b2725b7df427f8e40c453f7e
Basil <basil1987@gmail.com>
2023-11-09 11:46:52 +0530

    2023-11-07-Beginner's-Guide-to-DevOps.md

--- blog/sw.js
+++ blog/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/blog/css/pixyll.css?202311091034",
+        "/blog/css/pixyll.css?202311091146",
         "/blog/"
       ]);
     })
