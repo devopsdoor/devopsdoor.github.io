666c54c27c1e34987b8519311d5318882baa09ac
Basil <basil1987@gmail.com>
2024-01-22 18:35:28 +0530

    2024-01-15-The Evolution of DevOps.md

--- blog/scripts/buildpush.sh
+++ blog/scripts/buildpush.sh
@@ -7,6 +7,7 @@ bundle exec jekyll build
 cp -prf _site/* $DEVOPSDOOR_HOME/$BLOG_URL
 rm $DEVOPSDOOR_HOME/$BLOG_URL/robots.txt
 cd $DEVOPSDOOR_HOME
+cp -prf $BLOG_URL/sitemap.xml ./
 git add $BLOG_URL
 git commit -m "$LATEST_BLOG"
 git push origin main
