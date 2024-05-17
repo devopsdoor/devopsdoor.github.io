512ed3e2a15010cdaa850323240d68a39f71cba8
Basil <basil1987@gmail.com>
2024-05-17 15:55:54 +0530

    2024-05-07-Securing Cloud Workloads with DevSecOps Practices.md

--- blog/sw.js
+++ blog/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/blog/css/pixyll.css?202405151751",
+        "/blog/css/pixyll.css?202405171555",
         "/blog/"
       ]);
     })
