07ba561bb6f1b57f7ff061c3c15790ea8090e85c
Basil <basil1987@gmail.com>
2024-06-10 18:46:55 +0530

    2024-06-01-Traditional IT Roles.md

--- blog/sw.js
+++ blog/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/blog/css/pixyll.css?202405171555",
+        "/blog/css/pixyll.css?202406101846",
         "/blog/"
       ]);
     })
